a6aebb2e43ef138ad7eb66488b274b350d02f091
adviro22_laptop <ajosueramos@gmail.com>
2024-12-17 21:15:45 -0500

    Primer Commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args){
+        byte edad = 23;
+        int numeroGrande = 1000;
+        float numero = 15.5f;
+        boolean isActivate = true;
+        char nombre = 'J';
+        String nombre2 = "Josue Ortiz";
+        int length = nombre2.length();
+
+        System.out.println("Hola Mundo");
+        System.out.println(edad);
+        System.out.println(numeroGrande);
+        System.out.println(numero);
+        System.out.println(isActivate);
+        System.out.println(nombre);
+        System.out.println(nombre2);
+        System.out.println(length);
+
+        String text = "Hola Mundo";
+        String texto = text.replace("Hola", "Chao");
+        System.out.println(texto);
+
+    }
+}
